acb33a17163a89fd7760237b080b2e19555f5b53
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-05-23 09:23:24 +0200

    0.2 [react_advanced] implement sidebar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Navbar.module.scss';
 import {AppLink} from "shared/ui/AppLink/AppLink";
-import {ThemeSwitcher} from "widgets/ThemeSwitcher";
 
 interface NavbarProps {
     className?: string;
@@ -14,9 +13,6 @@ export const Navbar = ({ className }: NavbarProps) => {
             <div className={cls.links}>
                 <AppLink className={cls.linkItem} to={'/'}>Main</AppLink>
                 <AppLink className={cls.linkItem} to={'/about'}>About</AppLink>
-            </div>
-            <div className={cls.widgets}>
-                <ThemeSwitcher className='nonBorder'/>
             </div>
         </div>
     );
