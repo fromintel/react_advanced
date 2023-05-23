c2bdfe5a80f5156c1c4e42a7de5c06b9d2d1c346
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-05-23 22:29:45 +0200

    0.2 [react_advanced] create lang switcher component

---
+++ src/widgets/LangSwitcher/ui/LangSwitcher.tsx
@@ -0,0 +1,27 @@
+import React, {FC} from "react"
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from "./LangSwitcher.module.scss";
+import {useTranslation} from "react-i18next";
+import {Button, ThemeButton} from "shared/ui/Button/Button";
+
+interface LangSwitcherProps {
+    className?: string;
+}
+
+export const LangSwitcher: FC<LangSwitcherProps> = ({className}: LangSwitcherProps) => {
+    const { t, i18n } = useTranslation();
+
+    const toggle = () => {
+        i18n.changeLanguage(i18n.language === 'en' ? 'ua' : 'en');
+    }
+
+    return (
+        <Button
+            theme={ThemeButton.CLEAR}
+            onClick={toggle}
+            className={classNames(cls.LangSwitcher , {}, [className])}
+        >
+            { t('language_btn') }
+        </Button>
+    );
+};
