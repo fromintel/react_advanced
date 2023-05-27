012c4fd4e383b11575655a1035b3493b072a8a13
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-05-27 10:31:37 +0200

    0.2 [react_advanced] add jest config and setup some rules for jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
   extends: [
     'plugin:react/recommended',
@@ -25,7 +26,10 @@ module.exports = {
     'react/jsx-indent': [2, 2],
     'react/jsx-indent-props': [2, 2],
     indent: [2, 2],
-    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.tsx'] }],
+    'react/jsx-filename-extension':
+        [2,
+          { extensions: ['.js', '.jsx', '.tsx'] },
+        ],
     'import/no-unresolved': 'off',
     'import/prefer-default-export': 'off',
     'no-unused-vars': 'warn',
@@ -38,7 +42,11 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     'no-underscore-dangle': 'off',
     'no-redeclare': 'off',
-    'i18next/no-literal-string': ['warn', { markupOnly: true, ignoreAttribute: ['stroke'] }],
+    'i18next/no-literal-string':
+        ['warn',
+          { markupOnly: true, ignoreAttribute: ['stroke'] },
+        ],
+    'max-len': ['error', { ignoreComments: true }],
   },
   globals: {
     _IS_DEV_: true,
