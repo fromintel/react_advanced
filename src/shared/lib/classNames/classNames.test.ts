d3e5bf30c84cb0be72249b7d666852e632522f74
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-05-27 10:33:20 +0200

    0.2 [react_advanced] create the first tests for the classNames utility

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,46 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+  test('should return class with only one first param', () => {
+    expect(classNames('someClass')).toBe('someClass');
+  });
+
+  test('should return class with additional classes', () => {
+    const expectedResult = 'someClass className1 className2';
+    expect(classNames('someClass', {}, ['className1', 'className2']))
+      .toBe(expectedResult);
+  });
+
+  test('should return class with additional classes and modifications', () => {
+    const expectedResult = 'someClass className1 className2 hovered scrollable';
+    expect(classNames(
+      'someClass',
+      { hovered: true, scrollable: true },
+      ['className1', 'className2'],
+    )).toBe(expectedResult);
+  });
+
+  test(
+    'should return classes and modifications where one of them has false',
+    () => {
+      const expectedResult = 'someClass className1 className2 hovered';
+      expect(classNames(
+        'someClass',
+        { hovered: true, scrollable: false },
+        ['className1', 'className2'],
+      )).toBe(expectedResult);
+    },
+  );
+
+  test(
+    'should return class and modifications where one of them has undefined',
+    () => {
+      const expectedResult = 'someClass className1 className2 scrollable';
+      expect(classNames(
+        'someClass',
+        { hovered: undefined, scrollable: true },
+        ['className1', 'className2'],
+      )).toBe(expectedResult);
+    },
+  );
+});
