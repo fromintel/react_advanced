26c64f7222d01bca467e06da4e58153129399ffd
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-05-28 14:42:03 +0200

    0.3 [react_advanced/storybook] create stories for app link component

---
+++ src/shared/ui/AppLink/AppLink.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { AppLink, AppLinkTheme } from './AppLink';
+
+const meta: Meta<typeof AppLink> = {
+  title: 'shared/AppLink',
+  component: AppLink,
+  tags: ['autodocs'],
+  args: {
+    to: '/',
+    children: 'App Link',
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof AppLink>;
+
+export const Primary: Story = {
+  args: {
+    theme: AppLinkTheme.PRIMARY,
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    theme: AppLinkTheme.SECONDARY,
+  },
+};
