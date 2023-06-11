1c12b75fb65b29eff54088736126068e0f2196a0
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-06-11 13:23:12 +0200

    0.4 [react_advanced/auth_part2] update stories for theme switcher

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.stories.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.stories.tsx
@@ -1,4 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
 import { ThemeSwitcher } from './ThemeSwitcher';
 
 const meta: Meta<typeof ThemeSwitcher> = {
@@ -9,6 +11,12 @@ const meta: Meta<typeof ThemeSwitcher> = {
 export default meta;
 type Story = StoryObj<typeof ThemeSwitcher>;
 
-export const Primary: Story = {
+export const Light: Story = {
   args: {},
+  decorators: [ThemeDecorator(Theme.LIGHT)],
+};
+
+export const Dark: Story = {
+  args: {},
+  decorators: [ThemeDecorator(Theme.DARK)],
 };
