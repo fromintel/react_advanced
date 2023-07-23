340d87e403a771d01c1f98e519f36b32956edbb9
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-07-23 20:47:07 +0200

    0.6 [react_advanced/profile_testing] implement unit tests for update profile data

---
+++ src/entities/Profile/model/services/updateProfileData/updateProfileData.test.ts
@@ -0,0 +1,61 @@
+import { TestAsyncThunk } from 'shared/lib/tests/testAsyncThunk/testAsyncThunk';
+import { Countries } from 'entities/Countries';
+import { Currency } from 'entities/Currency';
+import { ValidateProfileError } from 'entities/Profile/model/types/profile';
+import { updateProfileData } from './updateProfileData';
+
+const data = {
+  firstname: 'Anton',
+  lastname: 'Zolotukhin',
+  age: 27,
+  country: Countries.UKRAINE,
+  city: 'Hannover',
+  currency: Currency.UAH,
+  username: 'zoltan',
+};
+
+describe('updateProfileData.test', () => {
+  test('should success update profile data', async () => {
+    const thunk = new TestAsyncThunk(updateProfileData, {
+      profile: {
+        form: data,
+      },
+    });
+    thunk.api.put.mockReturnValue(Promise.resolve({ data }));
+    const result = await thunk.callThunk();
+
+    expect(thunk.api.put).toHaveBeenCalled();
+    expect(result.meta.requestStatus).toBe('fulfilled');
+    expect(result.payload).toEqual(data);
+  });
+
+  test('should receive error while updating profile data', async () => {
+    const thunk = new TestAsyncThunk(updateProfileData, {
+      profile: {
+        form: data,
+      },
+    });
+    thunk.api.put.mockReturnValue(Promise.resolve({ status: 403 }));
+    const result = await thunk.callThunk();
+
+    expect(result.meta.requestStatus).toBe('rejected');
+    expect(result.payload).toEqual([
+      ValidateProfileError.SERVER_ERROR,
+    ]);
+  });
+
+  test('should receive error related to incorrect data from user', async () => {
+    const thunk = new TestAsyncThunk(updateProfileData, {
+      profile: {
+        form: { ...data, firstname: '' },
+      },
+    });
+    thunk.api.put.mockReturnValue(Promise.resolve({ status: 403 }));
+    const result = await thunk.callThunk();
+
+    expect(result.meta.requestStatus).toBe('rejected');
+    expect(result.payload).toEqual([
+      ValidateProfileError.INCORRECT_USER_DATA,
+    ]);
+  });
+});
