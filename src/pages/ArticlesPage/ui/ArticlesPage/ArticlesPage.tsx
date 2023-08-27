9daca96f741f70e3552a1ebfbc9c70942ec58275
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-08-27 16:05:49 +0200

    0.7 [react_advanced/article_implementation_part1] Start to implement new flow - articles

---
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -0,0 +1,20 @@
+import { FC, memo } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import cls from './ArticlesPage.module.scss';
+
+interface ArticlesPageProps {
+    className?: string;
+}
+
+const ArticlesPage: FC<ArticlesPageProps> = ({ className }: ArticlesPageProps) => {
+  const { t } = useTranslation('article');
+
+  return (
+    <div className={classNames(cls.ArticlesPage, {}, [className])}>
+      {t('article_page')}
+    </div>
+  );
+};
+
+export default memo(ArticlesPage);
--- src/widgets/Sidebar/model/items.ts
+++ src/widgets/Sidebar/model/items.ts
@@ -2,6 +2,7 @@ import React from 'react';
 import HomeIcon from 'shared/assets/icons/house.svg';
 import AboutIcon from 'shared/assets/icons/info.svg';
 import UserIcon from 'shared/assets/icons/circle-user.svg';
+import ArticleIcon from 'shared/assets/icons/article_regular.svg';
 import { RoutePath } from 'shared/config/routesConfig/routesConfig';
 
 export interface SidebarItemType {
@@ -26,6 +27,12 @@ export const SidebarItemsList: SidebarItemType[] = [
     path: RoutePath.profile,
     Icon: UserIcon,
     text: 'profile_nav_page',
+    authOnly: true,
+  },
+  {
+    path: RoutePath.articles,
+    Icon: ArticleIcon,
+    text: 'article_nav_page',
     authOnly: true,
   },
 ];
