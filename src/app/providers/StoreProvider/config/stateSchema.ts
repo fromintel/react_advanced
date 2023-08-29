65266174c1abd86e5b716e83bf4ef4dfaca48283
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-08-29 23:40:54 +0200

    0.7 [react_advanced/article_implementation_part2] Create some entities related to the article

--- src/app/providers/StoreProvider/config/stateSchema.ts
+++ src/app/providers/StoreProvider/config/stateSchema.ts
@@ -8,6 +8,7 @@ import { ProfileSchema } from 'entities/Profile';
 import { AxiosInstance } from 'axios';
 import { NavigateOptions, To } from 'react-router-dom';
 import { Dispatch } from 'redux';
+import { ArticleDetailsSchema } from 'entities/Article';
 
 export interface StateSchema {
     counter: CounterSchema;
@@ -15,7 +16,8 @@ export interface StateSchema {
 
     // async reducers
     loginForm?: LoginSchema;
-    profile: ProfileSchema;
+    profile?: ProfileSchema;
+    articleDetails?: ArticleDetailsSchema;
 }
 
 export type StateSchemaKey = keyof StateSchema;
