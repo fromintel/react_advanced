bc1dd16ddedd9c9c2232b00c601535326b317895
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-09-17 22:49:43 +0200

    0.7 [react_advanced/comments_implementation_part1] export some entities from the article details page flow

--- src/pages/ArticleDetailsPage/index.ts
+++ src/pages/ArticleDetailsPage/index.ts
@@ -1,3 +1,7 @@
 export {
   ArticleDetailsPageAsync as ArticleDetailsPage,
 } from './ui/ArticleDetailsPage/ArticleDetailsPage.async';
+
+export { ArticleDetailsCommentsSchema } from './model/types/ArticleDetailsCommentsSchema';
+
+export { getArticleCommentsError, getArticleCommentsIsLoading } from './model/selectors/comments';
