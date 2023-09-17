8c1f36048893e7f87e15d6a55dfe1a44b3a1f357
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-09-17 13:20:43 +0200

    0.7 [react_advanced/article_implementation_part2] Update article code block component

--- src/entities/Article/ui/ArticleCodeBlockComponent/ArticleCodeBlockComponent.tsx
+++ src/entities/Article/ui/ArticleCodeBlockComponent/ArticleCodeBlockComponent.tsx
@@ -1,19 +1,17 @@
 import { FC, memo } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { useTranslation } from 'react-i18next';
+import { Code } from 'shared/ui/Code/Code';
+import { ArticleCodeBlock } from '../../model/types/article';
 import cls from './ArticleCodeBlockComponent.module.scss';
 
 interface ArticleCodeBlockComponentProps {
     className?: string;
+    block: ArticleCodeBlock;
 }
 
 export const ArticleCodeBlockComponent:
-    FC<ArticleCodeBlockComponentProps> = memo(({ className }: ArticleCodeBlockComponentProps) => {
-      const { t } = useTranslation('article-details');
-
-      return (
-        <div className={classNames(cls.ArticleCodeBlockComponent, {}, [className])}>
-          <p>{t('article_code_block_component')}</p>
-        </div>
-      );
-    });
+    FC<ArticleCodeBlockComponentProps> = memo(({ className, block }: ArticleCodeBlockComponentProps) => (
+      <div className={classNames(cls.ArticleCodeBlockComponent, {}, [className])}>
+        <Code text={block.code} />
+      </div>
+    ));
