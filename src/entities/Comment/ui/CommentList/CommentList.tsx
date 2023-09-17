e1d2f3c17612c4b7c39281600d4313de1b0a1533
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-09-17 22:53:19 +0200

    0.7 [react_advanced/comments_implementation_part1] Implement comment list component

---
+++ src/entities/Comment/ui/CommentList/CommentList.tsx
@@ -0,0 +1,33 @@
+import { FC, memo } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Comment } from 'entities/Comment';
+import { useTranslation } from 'react-i18next';
+import { Text } from 'shared/ui/Text/Text';
+import cls from './CommentList.module.scss';
+import { CommentCard } from '../CommentCard/CommentCard';
+
+interface CommentListProps {
+    className?: string;
+    comments?: Comment[];
+    isLoading?: boolean;
+}
+
+export const CommentList: FC<CommentListProps> = memo((props: CommentListProps) => {
+  const { className, comments, isLoading } = props;
+  const { t } = useTranslation();
+
+  return (
+    <div className={classNames(cls.CommentList, {}, [className])}>
+      { comments?.length
+        ? comments.map((comment) => (
+          <CommentCard
+            key={comment.id}
+            isLoading={isLoading}
+            className={cls.commentBlock}
+            comment={comment}
+          />
+        ))
+        : <Text title={t('comments_error_title')} text={t('comments_error_subtitle')} /> }
+    </div>
+  );
+});
