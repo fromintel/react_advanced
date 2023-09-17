23274ffab2c58d714a995d518a277a7b284787fe
Anton Zolotukhin <35686159+fromintel@users.noreply.github.com>
2023-09-18 00:13:54 +0300

    0.7 [react_advanced/comments_implementation_part1] Implement new flow - comments (part 1)

---
+++ src/entities/Comment/ui/CommentCard/CommentCard.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { CommentCard } from './CommentCard';
+
+const meta: Meta<typeof CommentCard> = {
+  title: 'entities/CommentCard',
+  component: CommentCard,
+};
+
+export default meta;
+type Story = StoryObj<typeof CommentCard>;
+
+export const Light: Story = {
+  args: {
+    comment: {
+      id: '1',
+      text: 'Comment 1',
+      user: {
+        id: '1',
+        username: 'username 1',
+      },
+    },
+  },
+  decorators: [ThemeDecorator(Theme.LIGHT)],
+};
+
+export const Dark: Story = {
+  args: {
+    comment: {
+      id: '1',
+      text: 'Comment 1',
+      user: {
+        id: '1',
+        username: 'username 1',
+      },
+    },
+  },
+  decorators: [ThemeDecorator(Theme.DARK)],
+};
