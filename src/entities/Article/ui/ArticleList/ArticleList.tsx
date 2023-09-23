0d755d341648dae3bd02c860e9afb1e3265b6d5a
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-09-23 16:22:26 +0200

    0.8 [react_advanced/article_implementation_part4] Create a new component - article list

---
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -0,0 +1,57 @@
+import { FC, memo } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { ArticleListItemSkeleton } from '../../ui/ArticleListItem/ArticleListItemSkeleton';
+import { ArticleListItem } from '../ArticleListItem/ArticleListItem';
+import { Article, ArticleView } from '../../model/types/article';
+import cls from './ArticleList.module.scss';
+
+interface ArticleListProps {
+    className?: string;
+    articles: Article[];
+    isLoading?: boolean;
+    view?: ArticleView;
+}
+
+const getSkeletons = (view: ArticleView) => new Array(view === ArticleView.TILE ? 9 : 3)
+  .fill(0)
+  .map((item, index) => (
+    <ArticleListItemSkeleton
+      className={cls.card}
+      key={index}
+      view={view}
+    />
+  ));
+
+export const ArticleList: FC<ArticleListProps> = memo((props: ArticleListProps) => {
+  const {
+    className,
+    articles,
+    view = ArticleView.LIST_ITEM,
+    isLoading,
+  } = props;
+
+  if (isLoading) {
+    return (
+      <div className={classNames(cls.ArticleList, {}, [className, cls[view]])}>
+        {getSkeletons(view)}
+      </div>
+    );
+  }
+
+  const renderArticle = (article: Article) => (
+    <ArticleListItem
+      key={article.id}
+      article={article}
+      view={view}
+      className={cls.card}
+    />
+  );
+
+  return (
+    <div className={classNames(cls.ArticleList, {}, [className, cls[view]])}>
+      {articles.length > 0
+        ? articles.map(renderArticle)
+        : null}
+    </div>
+  );
+});
