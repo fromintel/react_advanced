273900ba63da560a3f55a21d4da405f492a8fad5
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-09-23 16:21:28 +0200

    0.8 [react_advanced/article_implementation_part4] Update exports for article entity

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -2,7 +2,10 @@ export {
   ArticleDetails,
 } from './ui/ArticleDetails/ArticleDetails';
 
+export { ArticleView } from './model/types/article';
 export type { Article } from './model/types/article';
 export type { ArticleDetailsSchema } from './model/types/articleDetailsSchema';
 
 export { getArticleDetailsData } from './model/selectors/articleDetails';
+
+export { ArticleList } from './ui/ArticleList/ArticleList';
