e07986781e25fe414872e5baf6594a737d5f6886
Anton Zolotukhin <azolotukhin@lenovo.com>
2023-09-23 23:36:21 +0200

    0.8 [react_advanced/article_implementation_part6] Create infinity scroll hook

---
+++ src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
@@ -0,0 +1,36 @@
+import { MutableRefObject, useEffect } from 'react';
+
+export interface UseInfiniteScrollOptions {
+    callback?: () => void;
+    triggerRef: MutableRefObject<HTMLElement>;
+    wrapperRef: MutableRefObject<HTMLElement>;
+}
+
+export function useInfiniteScroll({ callback, triggerRef, wrapperRef }: UseInfiniteScrollOptions) {
+  useEffect(() => {
+    let observer: IntersectionObserver | null = null;
+    const wrapperElement = wrapperRef.current;
+    const triggerElement = triggerRef.current;
+
+    const options = {
+      root: wrapperElement,
+      rootMargin: '2px',
+      threshold: 1.0,
+    };
+
+    observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) {
+        callback?.();
+      }
+    }, options);
+
+    observer.observe(triggerElement);
+
+    return () => {
+      if (observer && triggerElement) {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        observer.unobserve(triggerElement);
+      }
+    };
+  }, [callback, triggerRef, wrapperRef]);
+}
